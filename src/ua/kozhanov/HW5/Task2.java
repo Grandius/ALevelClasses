9315003b5e7681ab359712c83ea0c0111408b494
Grandius <gradius777@gmail.com>
2021-05-15 19:17:58 +0300

    Initial commit of HW5 Task 2

---
+++ src/ua/kozhanov/HW5/Task2.java
@@ -0,0 +1,47 @@
+package ua.kozhanov.HW5;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Task2 {
+
+    //Проверить заданный массив на упорядоченность по невозрастанию
+    public static void main(String[] args) {
+
+        int[] array = new int[10];
+        fillArray(array, 50);
+        System.out.println(Arrays.toString(array));
+        System.out.println("The array is properly descending: " + checkForDesc(array));
+
+        //to show that the algorithm works
+        int[] testArray = {27, 19, 15, 9, 6, 4, 1, 0, -1};
+        System.out.println(Arrays.toString(testArray));
+        System.out.println("The array is properly descending: " + checkForDesc(testArray));
+
+
+    }
+
+    static int[] fillArray(int[] inputArray, int bound) {
+
+        for (int i = 0; i < inputArray.length; i++) {
+
+            inputArray[i] = new Random().nextInt(bound);
+
+        }
+        return inputArray;
+    }
+
+    static boolean checkForDesc(int[] inputArray) {
+
+        boolean[] checkArray = new boolean[inputArray.length - 1];
+        boolean temp = true;
+
+        for (int i = 1; i < inputArray.length; i++) {
+
+            checkArray[i - 1] = inputArray[i - 1] >= inputArray[i];
+            temp = temp && checkArray[i - 1];
+
+        }
+        return temp;
+    }
+}
