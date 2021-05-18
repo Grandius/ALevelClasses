bd53180364be0f684c367cda32ec9e797c58d13c
Grandius <gradius777@gmail.com>
2021-05-18 19:07:41 +0300

    Cleanup of HW6 Task 1

--- src/ua/kozhanov/HW6/Phone.java
+++ src/ua/kozhanov/HW6/Phone.java
@@ -6,6 +6,26 @@ public class Phone {
     private String model;
     private double weight;
 
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
     public String getNumber() {
         return number;
     }
@@ -30,10 +50,7 @@ public class Phone {
     void receiveCall(String name) {
         System.out.println("Звонит " + name);
     }
-
-    void receiveCall(String name, String number) {
-        System.out.println("Calling " + name + " with number" + number);
-    }
+    
 
     @Override
     public String toString() {
--- src/ua/kozhanov/HW6/Task1.java
+++ src/ua/kozhanov/HW6/Task1.java
@@ -6,19 +6,20 @@ public class Task1 {
 
         Phone phone1 = new Phone("784513323643", "Xiaomi", 567.9);
         Phone phone2 = new Phone("13456765498765", "Samsung");
-        Phone phone3 = new Phone("","",0);
+        Phone phone3 = new Phone();
+        System.out.println("Empty phone3: " + phone3);
+        phone2.setWeight(56.7);
+        phone3.setNumber("38045147712578");
+        phone3.setModel("Realme");
+        phone3.setWeight(187.89);
 
 
         System.out.println("Phone 1 number is " + phone1.getNumber());
         phone1.receiveCall("Bob");
-
         System.out.println("Phone 2 number is " + phone2.getNumber());
         phone2.receiveCall("Alice");
-
         System.out.println("Phone 3 number is " + phone3.getNumber());
         phone3.receiveCall("John");
-
-
         System.out.println("Testing toString: ");
         System.out.println(phone1);
         System.out.println(phone2);
